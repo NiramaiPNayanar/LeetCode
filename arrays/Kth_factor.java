aa3e9ed0a764b88396adab3bce0c0a06cf47eda5
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-01-07 20:03:57 +0530

    Create Kth_factor.java

---
+++ arrays/Kth_factor.java
@@ -0,0 +1,28 @@
+/*
+Name:-  The kth Factor of n
+complexity - O(n)
+bestcase - O(1)
+
+beats 100% of the codes
+
+code :-
+*/
+class Solution {
+    public int kthFactor(int n, int k) {
+        int ans = 0;
+        for(int i =1; i<=n ;i++)
+        {
+            if(n%i ==0)
+            {
+                ans++;
+            }
+
+            if(ans == k)
+            {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
