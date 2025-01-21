a0f3b67ce06869736b3867def1e5ab71bbc6ca64
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-01-21 20:51:27 +0530

    Create validate_stack_sequences.py

---
+++ Stacks/validate_stack_sequences.py
@@ -0,0 +1,28 @@
+''' 
+152 / 152 test cases passed.
+Status: Accepted
+Runtime: 4 ms
+Memory Usage: 12.5 MB
+
+Best Time Complexity - O(n)
+
+code :
+'''
+
+class Solution(object):
+    def validateStackSequences(self, pushed, popped):
+        """
+        :type pushed: List[int]
+        :type popped: List[int]
+        :rtype: bool
+        """
+        stack = []
+        i=0
+
+        for num in pushed:
+            stack.append(num)
+            while len(stack) >0 and stack[len(stack)-1] == popped[i]:
+                stack.pop()
+                i+=1
+        return True if len(stack) ==0 else False
+        
