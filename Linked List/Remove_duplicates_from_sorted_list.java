8eb4ea1a3223b055c363e792504ac39933485b00
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-01-27 11:05:38 +0530

    Create Remove_duplicates_from_sorted_list.java

---
+++ Linked List/Remove_duplicates_from_sorted_list.java
@@ -0,0 +1,39 @@
+/*
+
+Difficulty : Easy
+
+168 / 168 test cases passed.
+Status: Accepted
+Runtime: 0 ms
+Memory Usage: 16 MB
+
+Complexity : O(n)
+
+link :- https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+Code :
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* current = head;
+        while (current && current->next) {
+            if (current->val == current->next->val) {
+                current->next = current->next->next;
+            } else {
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
