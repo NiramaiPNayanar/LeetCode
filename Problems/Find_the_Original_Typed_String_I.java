488e1d9753909acdb158d12276f9b08b88461fc2
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-07-01 11:59:49 +0530

    Create Find_the_Original_Typed_String_I.java

---
+++ Problems/Find_the_Original_Typed_String_I.java
@@ -0,0 +1,30 @@
+/*
+
+780 / 780 test cases passed.
+Status: Accepted
+Runtime: 0 ms
+Memory Usage: 42.2 MB
+
+Best Case : O(n)
+Average Case : O(n)
+Worst Case : O(n)
+
+Difficulty : Easy 
+Link : https://leetcode.com/problems/find-the-original-typed-string-i/description/
+
+
+*/
+
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int count = 1;
+        for (int i =0 ; i < word.length() -1; i ++){
+            if (word.charAt(i) == word.charAt(i+1)) {
+                count +=1;
+            }
+        }
+
+        return count;
+    }
+}
