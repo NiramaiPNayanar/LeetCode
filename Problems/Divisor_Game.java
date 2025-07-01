e08291cce1d5538b45d1ff1d535f1d6d0d715a8c
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-07-01 12:17:04 +0530

    Update Divisor_Game.java

--- Problems/Divisor_Game.java
+++ Problems/Divisor_Game.java
@@ -5,10 +5,15 @@ Status: Accepted
 Runtime: 0 ms
 Memory Usage: 40.6 MB
 
+Time complexity - O(1)
+Space complexity - O(1)
+bestcase - O(1) 
+
 Best case complexity : O(1)
 Worst case complexity : O(1)
 Average case complexity : O(1)
 
+Name : Divisor Game
 Difficulty : Easy
 Link : https://leetcode.com/problems/divisor-game/description/
 
