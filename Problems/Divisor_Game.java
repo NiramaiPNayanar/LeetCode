7b5abc443bd298b34938a677d75eae0abc57cb87
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-07-01 11:33:10 +0530

    Create Divisor_Game.java

---
+++ Problems/Divisor_Game.java
@@ -0,0 +1,24 @@
+/*
+
+40 / 40 test cases passed.
+Status: Accepted
+Runtime: 0 ms
+Memory Usage: 40.6 MB
+
+Best case complexity : O(1)
+Worst case complexity : O(1)
+Average case complexity : O(1)
+
+Difficulty : Easy
+Link : https://leetcode.com/problems/divisor-game/description/
+
+*/
+
+
+
+
+class Solution {
+    public boolean divisorGame(int n) {
+        return n%2 == 0; 
+    }
+}
