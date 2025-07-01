d71c60e6b45673dd4f658fe74928408f25ae76d5
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-07-01 12:20:02 +0530

    Update Maximum_Repeating_Substring.java

--- Problems/Maximum_Repeating_Substring.java
+++ Problems/Maximum_Repeating_Substring.java
@@ -1,11 +1,19 @@
 /*
 
+Beats 100% of the solutions 
+
 212 / 212 test cases passed.
 Status: Accepted
 Runtime: 0 ms
 Memory Usage: 41.8 MB
 
 Complexity : O(n)
+
+Time complexity - O(n)
+Space complexity - O(1)
+Best Case - O(1)
+Average Case - O(1)
+Worst Case - O(1)
 
 Difficulty : Easy
 link : https://leetcode.com/problems/maximum-repeating-substring/
