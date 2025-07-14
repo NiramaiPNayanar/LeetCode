a9795fe50aa82f107f7c7c5b24d8611ea781277d
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-07-14 21:20:39 +0530

    Update Convert_Binary_Number_in_a_Linked_List_to_Integer.java

--- Linked List/Convert_Binary_Number_in_a_Linked_List_to_Integer.java
+++ Linked List/Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -13,3 +13,23 @@ Runtime: 0 ms
 Memory Usage: 41.5 MB
 
 */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int value = 0;
+        while(head != null){
+        value = value*2 +head.val; 
+        head = head.next;  
+        }
+        return value;        
+    }
+}
